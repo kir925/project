85d9afc8543c7f4ddc88665e69875dc5ed6350f0
kir925 <kriper.men2015@mail.ru>
2024-06-26 01:02:42 +0800

    Update user.py

--- user.py
+++ user.py
@@ -1,4 +1,3 @@
-# user.py
 import paho.mqtt.client as mqtt_client
 
 broker = "broker.emqx.io"
@@ -8,14 +7,28 @@ def on_message(client, userdata, message):
     data = str(message.payload.decode("utf-8"))
     print("Received message:", data)
 
-client = mqtt_client.Client('user')
+client = mqtt_client.Client('user')  # Не указываем явно протокол
 client.on_message = on_message
 
-client.connect(broker)
-client.loop_start()
+try:
+    client.connect(broker)
+    client.loop_start()
 
-receiver_name = input("Enter receiver name to subscribe: ")
-client.subscribe(f"{topic_prefix}{receiver_name}")
+    receiver_name = input("Enter receiver name to subscribe: ")
+    client.subscribe(f"{topic_prefix}{receiver_name}")
 
-client.loop_forever()
+    while True:
+        pass  # Цикл ожидания сообщений, обработка происходит в on_message
+
+except KeyboardInterrupt:
+    print("Interrupted by user")
+    client.disconnect()
+
+except mqtt_client.MQTTException as mqtt_ex:
+    print(f"MQTT Exception occurred: {mqtt_ex}")
+    client.disconnect()
+
+except Exception as ex:
+    print(f"An unexpected error occurred: {ex}")
+    client.disconnect()
 

8a1ef46508f5526950f0147a2a0fc2c1a35351f5
kir925 <kriper.men2015@mail.ru>
2024-06-25 21:32:20 +0800

    Update mqtt_subscriber.py

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -1,1 +1,21 @@
+# mqtt_subscriber.py
+import paho.mqtt.client as mqtt_client
+import time
+
+broker = "broker.emqx.io"
+topic_prefix = "gnss/data/"
+
+def on_message(client, userdata, message):
+    data = str(message.payload.decode("utf-8"))
+    print("Received message:", data)
+
+client = mqtt_client.Client('subscriber')
+client.on_message = on_message
+
+client.connect(broker)
+client.loop_start()
+
+client.subscribe(f"{topic_prefix}#")
+
+client.loop_forever()
 

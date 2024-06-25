2be26128a138d2bf307494fcaf8f5d8e2ef40c4a
kir925 <kriper.men2015@mail.ru>
2024-06-26 01:01:16 +0800

    Update mqtt_publisher.py

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -3,11 +3,10 @@ import paho.mqtt.client as mqtt_client
 
 broker = "broker.emqx.io"
 topic_prefix = "gnss/data/"
+client = mqtt_client.Client(client_id='publisher', clean_session=True, userdata>
 
-client = mqtt_client.Client('publisher')
 client.connect(broker)
 client.loop_start()
 
 # Никаких дополнительных действий, просто оставляем клиента подключенным
 client.loop_forever()
-
